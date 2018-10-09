7966c942fbcf4b4bf29b243bcfd0d657c071e928
Birkett <BirkCh01@risk.regn.net>
2018-10-09 15:10:01 -0500

    updated to latest version of paged search grid component

--- prototype-app/src/app/shared/components/paged-search/paged-search-grid/paged-search-grid.component.spec.ts
+++ prototype-app/src/app/shared/components/paged-search/paged-search-grid/paged-search-grid.component.spec.ts
@@ -1,13 +1,23 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { DebugElement } from "@angular/core";
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from "@angular/core/testing";
+import { DebugElement, SimpleChange } from "@angular/core";
 import { By } from "@angular/platform-browser";
 
 import { WjGridModule, WjFlexGrid } from "wijmo/wijmo.angular2.grid";
 import { WjGridFilterModule } from "wijmo/wijmo.angular2.grid.filter";
 import { WjInputModule } from "wijmo/wijmo.angular2.input";
+import * as wjcGrid from "wijmo/wijmo.grid";
+import * as wjcGridFilter from "wijmo/wijmo.angular2.grid.filter";
+
+import * as _ from "lodash";
 
 import { PagedSearchGridComponent } from "./paged-search-grid.component";
 import { PagedSearchFactory } from "../../../../test-helpers/paged-search-factory";
+import { GridSelectionMode } from "../../../enumerations";
+import { GridColumnsComponent } from "../grid-columns/grid-columns.component";
+import { PagerComponent } from "../pager/pager.component";
+import { ColumnSearchFilter } from "../../../models";
+import { PagedSearchDefaults } from "../../../config/paged-search-defaults";
+import { nextTick } from "q";
 
 describe("PagedSearchGridComponent", () => {
   let component: PagedSearchGridComponent;
@@ -15,19 +25,30 @@ describe("PagedSearchGridComponent", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [PagedSearchGridComponent],
-      imports: [WjGridModule, WjGridFilterModule, WjInputModule]
+      declarations: [
+        PagedSearchGridComponent,
+        GridColumnsComponent,
+        PagerComponent
+      ],
+      imports: [
+        WjGridModule, 
+        WjGridFilterModule, 
+        WjInputModule
+      ]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PagedSearchGridComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should have gridSectionHeight set to 0 on initialization", () => {
+    expect(component.gridSectionHeight).toBe("0");
   });
 
   it("should have a valid paged search result when it is assigned to the component", () => {
@@ -35,7 +56,6 @@ describe("PagedSearchGridComponent", () => {
     component.pagedSearchResult = PagedSearchFactory.getPagedSearchResult();
 
     // ACT
-    fixture.detectChanges();
 
     // ASSERT
     expect(component.pagedSearchResult.columns.length).toEqual(10);
@@ -48,63 +68,203 @@ describe("PagedSearchGridComponent", () => {
     expect(component.pagedSearchResult.totalPages).toEqual(26);
   });
 
-  it("should have gridData when the paged search result is assigned to the component", () => {
+  it("should have gridData when the paged search result is assigned to the component", async(() => {
     // ARRANGE
     component.pagedSearchResult = PagedSearchFactory.getPagedSearchResult();
+    const simpleChanges = {
+      pagedSearchResult: new SimpleChange(null,PagedSearchFactory.getPagedSearchResult(), false),
+      resetGrid: new SimpleChange(null,false, false),
+      showEditButton: new SimpleChange(null,false, false),
+      enableBulkEdit: new SimpleChange(null,false, false),
+      selectionMode: new SimpleChange(null, GridSelectionMode.Row, false)
+    };
 
     // ACT
     fixture.detectChanges();
-    component.ngOnChanges();
+    component.ngOnChanges(simpleChanges);
 
     // ASSERT
-    expect(component.gridData.length).toEqual(50);
+    fixture.whenStable().then(() => { 
+      fixture.detectChanges();
+
+      expect(component.gridData.length).toEqual(50);
+      expect(component.gridSectionHeight).not.toBe("0");
+    });
+  }));
+
+  it("should have showGrid equal to true when the paged search result is assigned to the component", async(() => {
+    // ARRANGE
+    component.pagedSearchResult = PagedSearchFactory.getPagedSearchResult();
+    const simpleChanges = {
+      pagedSearchResult: new SimpleChange(null,PagedSearchFactory.getPagedSearchResult(), false),
+      resetGrid: new SimpleChange(null,false, false),
+      showEditButton: new SimpleChange(null,false, false),
+      enableBulkEdit: new SimpleChange(null,false, false),
+      selectionMode: new SimpleChange(null, GridSelectionMode.Row, false)
+    };
+
+    // ACT
+    fixture.detectChanges();
+    component.ngOnChanges(simpleChanges);
+
+    // ASSERT
+    fixture.whenStable().then(() => { 
+      fixture.detectChanges();
+
+      expect(component.showGrid).toEqual(true);
+    });
+  }));
+
+  describe("#onPageRequest", () => {
+    it("should have a new page number in the search request when called", () => {
+      component.gridSearchRequest.subscribe((value) => { 
+        let searchRequest = value; 
+        expect(searchRequest.page).not.toBe(1);
+      });
+
+      component.onPageRequest(2);
+    });
   });
 
-  it("should have valid filterColumns when the paged search result is assigned to the component", () => {
-    // ARRANGE
-    component.pagedSearchResult = PagedSearchFactory.getPagedSearchResult();
+  describe("#onFilterChanged", () => {
+    it('should emit a valid search request when filter event issued', () => {
 
-    // ACT
-    fixture.detectChanges();
-    component.ngOnChanges();
+      // TODO: Manually set up a filter and issue the filter request from the DOM
 
-    // ASSERT
-    expect(component.filterColumns.length).toEqual(10);
+      const mockEventArgs: wjcGrid.CellRangeEventArgs = {
+        cancel: false,
+        col: 1, // index of second column name in the filterColumns array member
+        data: {},
+        panel: null,
+        range: null,
+        row: 1,
+        _p: null,
+        _rng: null,
+        _data: null
+      };
+
+      component.gridSearchRequest.subscribe((value) => { 
+        let searchRequest = value; 
+        expect(searchRequest.filters).toBeTruthy();
+      });
+
+      component.onFilterChanged(mockEventArgs);
+    });
   });
 
-  it("should have valid gridColumns when the paged search result is assigned to the component", () => {
-    // ARRANGE
-    component.pagedSearchResult = PagedSearchFactory.getPagedSearchResult();
+  describe("#onSortedColumn", () => {
+    it('should emit a valid search request when sort event issued', async(() => {
 
-    // ACT
-    fixture.detectChanges();
-    component.ngOnChanges();
+      const mockEventArgs: wjcGrid.CellRangeEventArgs = {
+        cancel: true,
+        col: 1, // index of second column name in the filterColumns array member
+        data: {},
+        panel: null,
+        range: null,
+        row: 1,
+        _p: null,
+        _rng: null,
+        _data: null
+      };
+      
+      component.pagedSearchResult = PagedSearchFactory.getPagedSearchResult();
+      fixture.detectChanges();
 
-    // ASSERT
-    expect(component.gridColumns.length).toEqual(10);
-    expect(component.gridColumns[0].binding).toEqual("vendorId");
-    expect(component.gridColumns[0].format).toBeNull();
-    expect(component.gridColumns[0].header).toEqual("Vendor");
+      component.gridSearchRequest.subscribe((value) => { 
+        let searchRequest = value; 
+        expect(searchRequest.sortBy).not.toBe(PagedSearchDefaults.sortColumn);
+        expect(searchRequest.sortDirection).toBe("ASC");
+      });
+
+      fixture.whenStable().then(() => { 
+        component.onSortedColumn(mockEventArgs);
+      });
+    }));
   });
 
-  it("should have showGrid equal to true when the paged search result is assigned to the component", () => {
-    // ARRANGE
-    component.pagedSearchResult = PagedSearchFactory.getPagedSearchResult();
+  describe("#editRow", () => {
+    it("should emit a record edit when clicked", async(() => {
+      const recordToEdit = PagedSearchFactory.getPagedSearchResult().searchResults[0];
 
-    // ACT
-    fixture.detectChanges();
-    component.ngOnChanges();
+      component.editRecord.subscribe((value) => { 
+        let record = value; 
+        expect(record).toBeTruthy();
+        expect(record).toBe(recordToEdit);
+      });
 
-    // ASSERT
-    expect(component.showGrid).toEqual(true);
+      fixture.whenStable().then(() => { 
+        component.editRow(recordToEdit);
+      });
+    }));
+  });
+
+  describe('#GridFilters', () => {
+    it("should have valid filterColumns when the paged search result is assigned to the component", async(() => {
+      // ARRANGE
+      component.pagedSearchResult = PagedSearchFactory.getPagedSearchResult();
+      const simpleChanges = {
+        pagedSearchResult: new SimpleChange(null, PagedSearchFactory.getPagedSearchResult(), false),
+        resetGrid: new SimpleChange(null,false, false),
+        showEditButton: new SimpleChange(null,false, false),
+        enableBulkEdit: new SimpleChange(null,false, false),
+        selectionMode: new SimpleChange(null, GridSelectionMode.Row, false)
+      };
+
+      // ACT
+      fixture.detectChanges();
+      component.ngOnChanges(simpleChanges);
+
+      // ASSERT
+      fixture.whenStable().then(() => { 
+        fixture.detectChanges();
+
+        expect(component.filterColumns.length).toEqual(10);
+      });
+      
+    }));
+
+    it("#GridFilters should have valid filterColumns when the paged search result is assigned to the component", async(() => {
+      const simpleChanges = {
+        pagedSearchResult: new SimpleChange(null,PagedSearchFactory.getPagedSearchResult(), true)
+      };
+      
+      component.pagedSearchResult = PagedSearchFactory.getPagedSearchResult();
+      fixture.detectChanges();
+      component.ngOnChanges(simpleChanges);
+      fixture.detectChanges();
+
+      const debugElement: DebugElement = fixture.debugElement;
+      const gridDebugElement: DebugElement = debugElement.query(By.directive(WjFlexGrid));
+      const grid: WjFlexGrid = gridDebugElement.componentInstance;
+      grid.refreshCells(true);
+      const gridNativeElement: HTMLElement = gridDebugElement.nativeElement;
+
+      fixture.whenStable().then(() => { 
+        fixture.detectChanges();
+
+        expect(component.filter.filterColumns.length).toEqual(10);
+        _.forEach(component.filter.filterColumns, columnName => {
+          expect(component.filter.getColumnFilter(columnName)).toBeTruthy();
+          expect(component.filter.getColumnFilter(columnName).valueFilter).toBeTruthy();
+          // test each valueFilter.uniqueValues collection to ensure it matches the length of the columns.filterValues property
+        });
+      });
+    }));
   });
 
   describe("#WijmoGrid", () => {
     it(`should display the "Vendor" column header`, async(() => {
+      const simpleChanges = {
+        pagedSearchResult: new SimpleChange(null,PagedSearchFactory.getPagedSearchResult(), false),
+        // resetGrid: new SimpleChange(null,false, false),
+        // showEditButton: new SimpleChange(null,false, false),
+        // enableBulkEdit: new SimpleChange(null,false, false),
+        // selectionMode: new SimpleChange(null, GridSelectionMode.Row, false)
+      };
       
       component.pagedSearchResult = PagedSearchFactory.getPagedSearchResult();
       fixture.detectChanges();
-      component.ngOnChanges();
+      component.ngOnChanges(simpleChanges);
       fixture.detectChanges();
 
       const debugElement: DebugElement = fixture.debugElement;
@@ -113,20 +273,30 @@ describe("PagedSearchGridComponent", () => {
       grid.refreshCells(true);
       const gridNativeElement: HTMLElement = gridDebugElement.nativeElement;
 
-      fixture.detectChanges();
+      fixture.whenStable().then(() => { 
+        fixture.detectChanges();
 
-      const colHeaderCellElement: Element | null = gridNativeElement.querySelectorAll('.wj-colheaders .wj-cell.wj-header')[0];
-      expect(colHeaderCellElement).not.toBeNull();
-      if (colHeaderCellElement) {
-          expect(colHeaderCellElement.textContent).toEqual('Vendor');
-      }
+        const colHeaderCellElement: Element | null = gridNativeElement.querySelectorAll('.wj-colheaders .wj-cell.wj-header')[0];
+        expect(colHeaderCellElement).not.toBeNull();
+        if (colHeaderCellElement) {
+            expect(colHeaderCellElement.textContent).toEqual('Vendor');
+        }
+      });
     }));
     
     it(`should display the column headers`, async(() => {
+      const simpleChanges = {
+        pagedSearchResult: new SimpleChange(null,PagedSearchFactory.getPagedSearchResult(), false),
+        resetGrid: new SimpleChange(null,false, false),
+        showEditButton: new SimpleChange(null,false, false),
+        enableBulkEdit: new SimpleChange(null,false, false),
+        selectionMode: new SimpleChange(null, GridSelectionMode.Row, false)
+      };
+
       // TODO: Update this test to use the PagedSearchFactory.getPagedSearchResult() object to read out the columnHeaders from the columns collection for the assertions
       component.pagedSearchResult = PagedSearchFactory.getPagedSearchResult();
       fixture.detectChanges();
-      component.ngOnChanges();
+      component.ngOnChanges(simpleChanges);
       fixture.detectChanges();
 
       const debugElement: DebugElement = fixture.debugElement;
@@ -135,31 +305,41 @@ describe("PagedSearchGridComponent", () => {
       grid.refreshCells(true);
       const gridNativeElement: HTMLElement = gridDebugElement.nativeElement;
 
-      fixture.detectChanges();
+      fixture.whenStable().then(() => { 
+        fixture.detectChanges();
 
-      // TODO: The CSS structure of the grid does not match the example this test is based on..not sure why this works
-      const columnHeaders = gridNativeElement.querySelectorAll('.wj-colheaders .wj-cell.wj-header');
-      const colHeaderCellElement: Element | null = columnHeaders[0];
-      expect(colHeaderCellElement).not.toBeNull();
-      if (colHeaderCellElement) {
-          expect(colHeaderCellElement.textContent).toEqual('Vendor');
-          expect(columnHeaders[1].textContent).toEqual('Contract');
-          expect(columnHeaders[2].textContent).toEqual('Business Name');
-          expect(columnHeaders[3].textContent).toEqual('Court');
-          expect(columnHeaders[4].textContent).toEqual('File Type');
-          expect(columnHeaders[5].textContent).toEqual('Product');
-          expect(columnHeaders[6].textContent).toEqual('Goal');
-          expect(columnHeaders[7].textContent).toEqual('Threshold');
-          expect(columnHeaders[8].textContent).toEqual('Start Date');
-          expect(columnHeaders[9].textContent).toEqual('End Date');
-      }
+        // TODO: The CSS structure of the grid does not match the example this test is based on..not sure why this works
+        const columnHeaders = gridNativeElement.querySelectorAll('.wj-colheaders .wj-cell.wj-header');
+        const colHeaderCellElement: Element | null = columnHeaders[0];
+        expect(colHeaderCellElement).not.toBeNull();
+        if (colHeaderCellElement) {
+            expect(colHeaderCellElement.textContent).toEqual('Vendor');
+            expect(columnHeaders[1].textContent).toEqual('Contract');
+            expect(columnHeaders[2].textContent).toEqual('Business Name');
+            expect(columnHeaders[3].textContent).toEqual('Court');
+            expect(columnHeaders[4].textContent).toEqual('File Type');
+            expect(columnHeaders[5].textContent).toEqual('Product');
+            expect(columnHeaders[6].textContent).toEqual('Goal');
+            expect(columnHeaders[7].textContent).toEqual('Threshold');
+            expect(columnHeaders[8].textContent).toEqual('Start Date');
+            expect(columnHeaders[9].textContent).toEqual('End Date');
+        }
+      });
     }));
 
     it(`should render the first row of search results`, async(() => {
+      const simpleChanges = {
+        pagedSearchResult: new SimpleChange(null,PagedSearchFactory.getPagedSearchResult(), false),
+        resetGrid: new SimpleChange(null,false, false),
+        showEditButton: new SimpleChange(null,false, false),
+        enableBulkEdit: new SimpleChange(null,false, false),
+        selectionMode: new SimpleChange(null, GridSelectionMode.Row, false)
+      };
+
       // TODO: Update this test to read the first record from the PagedSearchFactory.getPagedSearchResult() object searchResults property for the assertions
       component.pagedSearchResult = PagedSearchFactory.getPagedSearchResult();
       fixture.detectChanges();
-      component.ngOnChanges();
+      component.ngOnChanges(simpleChanges);
       fixture.detectChanges();
 
       const debugElement: DebugElement = fixture.debugElement;
@@ -168,25 +348,28 @@ describe("PagedSearchGridComponent", () => {
       grid.refreshCells(true);
       const gridNativeElement: HTMLElement = gridDebugElement.nativeElement;
 
-      fixture.detectChanges();
+      fixture.whenStable().then(() => { 
 
-      // TODO: The CSS structure of the grid does not match the example this test is based on...0 - 9 are the column headers
-      const gridCells = gridNativeElement.querySelectorAll('.wj-cells .wj-cell');
-      console.log(`should render the first row of search results`, gridCells);
-      const vendorCellElement: Element | null = gridCells[10];
-      expect(vendorCellElement).not.toBeNull();
-      if (vendorCellElement) {
-          expect(vendorCellElement.textContent).toEqual('55');
-          expect(gridCells[11].textContent).toEqual('72');
-          expect(gridCells[12].textContent).toEqual('IN-STATE INVESTIGATIONS, LLC');
-          expect(gridCells[13].textContent).toEqual('AZAPAC1   ');
-          expect(gridCells[14].textContent).toEqual('CF');
-          expect(gridCells[15].textContent).toEqual('BPRC');
-          expect(gridCells[16].textContent).toEqual('0');
-          expect(gridCells[17].textContent).toEqual('0.95');
-          expect(gridCells[18].textContent).toEqual('07/12/2017');
-          expect(gridCells[19].textContent).toEqual('07/11/2018');
-      }
-  }));
+        fixture.detectChanges();
+  
+        // TODO: The CSS structure of the grid does not match the example this test is based on...0 - 9 are the column headers
+        const gridCells = gridNativeElement.querySelectorAll('.wj-cells .wj-cell');
+        console.log(`should render the first row of search results`, gridCells);
+        const vendorCellElement: Element | null = gridCells[10];
+        expect(vendorCellElement).not.toBeNull();
+        if (vendorCellElement) {
+            expect(vendorCellElement.textContent).toEqual('55');
+            expect(gridCells[11].textContent).toEqual('72');
+            expect(gridCells[12].textContent).toEqual('IN-STATE INVESTIGATIONS, LLC');
+            expect(gridCells[13].textContent).toEqual('AZAPAC1   ');
+            expect(gridCells[14].textContent).toEqual('CF');
+            expect(gridCells[15].textContent).toEqual('BPRC');
+            expect(gridCells[16].textContent).toEqual('0');
+            expect(gridCells[17].textContent).toEqual('0.95');
+            expect(gridCells[18].textContent).toEqual('07/12/2017');
+            expect(gridCells[19].textContent).toEqual('07/11/2018');
+        }
+      });
+    }));
   });
 });
--- prototype-app/src/app/shared/components/paged-search/paged-search-grid/paged-search-grid.component.ts
+++ prototype-app/src/app/shared/components/paged-search/paged-search-grid/paged-search-grid.component.ts
@@ -1,72 +1,207 @@
-import { Component, OnInit, OnChanges, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, ViewChild, Output, EventEmitter, SimpleChanges } from "@angular/core";
 
-import * as _ from 'lodash';
+import * as _ from "lodash";
 
-import * as wjcCore from 'wijmo/wijmo';
-import * as wjcGrid from 'wijmo/wijmo.grid';
-import * as wjcGridFilter from 'wijmo/wijmo.angular2.grid.filter';
+import * as wjcGrid from "wijmo/wijmo.grid";
 import { FilterType } from 'wijmo/wijmo.grid.filter';
+import * as wjcGridFilter from "wijmo/wijmo.angular2.grid.filter";
 
-import { PagedSearchResult, ColumnDefinition, FilterDefinitionModel, Column } from '../../../models';
+import {
+  PagedSearchResult,
+  PagedSearchRequest,
+  ColumnSearchFilter,
+  FilterDefinitionModel,
+  Column,
+  ColumnDefinition
+} from "../../../models";
+import { PagedSearchDefaults } from "../../../config/paged-search-defaults";
+import { GridSelectionMode } from "../../../enumerations";
 
 @Component({
-  selector: 'ln-paged-search-grid',
-  templateUrl: './paged-search-grid.component.html'
+  selector: "ln-paged-search-grid",
+  templateUrl: "./paged-search-grid.component.html"
 })
 export class PagedSearchGridComponent implements OnInit, OnChanges {
-  
-  @Input() pagedSearchResult: PagedSearchResult
-  @Input() resetGrid: boolean;
+
+  @Input() pagedSearchResult: PagedSearchResult;
+  @Input() resetGrid: boolean = false;
+  @Input() showEditButton: boolean = false;
+  @Input() enableBulkEdit: boolean = false; // TODO: This is stubbed for future use since not all applications will use the multiselect feature to edit
+  @Input() selectionMode: GridSelectionMode = GridSelectionMode.Row;
+
+  @Output() gridSearchRequest = new EventEmitter<PagedSearchRequest>();
+  @Output() editRecord = new EventEmitter<any>();
 
   showGrid: boolean = false;
+  gridSectionHeight: string = "0"; // used to control the height of the grid section DOM element
   totalResults: number;
   currentPage: number;
   totalPages: number;
 
   gridData: any[];
-  selectionMode = 'ListBox';
-  gridColumns: ColumnDefinition[] = []; 
-  
-  filterColumns: string[] = []; 
+
+  filterColumns: string[] = [];
+  columns: Column[] = new Array<Column>();
+  gridColumns: ColumnDefinition[] = [];
+
+  sortColumn: string = PagedSearchDefaults.sortColumn;
+  sortDirection: string = PagedSearchDefaults.sortDirection;
+
+  @ViewChild("grid") flexGrid: wjcGrid.FlexGrid;
+
   defaultFilterType = FilterType.Value;
   filterDefinitionCache: FilterDefinitionModel;
 
-  //@ViewChild('filter') filter: wjcGridFilter.WjFlexGridFilter;
-  @ViewChild('grid') flexGrid: wjcGrid.FlexGrid;
+  @ViewChild("filter") filter: wjcGridFilter.WjFlexGridFilter;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.resetGrid && changes.resetGrid.currentValue) {
+      this.showGrid = false;
+      this.resetGridSettingsToDefaults();
+    } 
+    if (changes.pagedSearchResult && changes.pagedSearchResult.currentValue) {
+      this.showGrid = this.pagedSearchResult.searchResults.length > 0;
+      this.setupAndConfigurGrid();     
+    }
   }
 
-  ngOnChanges() {
-    if (this.resetGrid) {
-      this.showGrid = false;
-      // logic to reset grid here
-    }
-    else if (this.pagedSearchResult) {
-      // get the list of columns from the PagedSearchResult object
-      this.filterColumns = _.map(this.pagedSearchResult.columns, 'columnId');
+  onFormatItem(e: wjcGrid.FormatItemEventArgs): void {
+    e.cancel = true;
 
-      // get the list of column definitions
-      this.gridColumns = this.generateGridColumnCollection(this.pagedSearchResult.columns);
+    // need to manually control the sort icon since sorting is being overridden
+    const panel = e.panel;
+    if (panel.cellType == wjcGrid.CellType.ColumnHeader) {
+      const column = panel.columns[e.col];
+      if (this.sortColumn == column.binding) {
+        if (this.sortDirection == "ASC") {
+          e.cell.innerHTML = column.header + ' <span class="wj-glyph-up"></span>';
+        } else {
+          e.cell.innerHTML = column.header + ' <span class="wj-glyph-down"></span>';
+        }
+      }
+    }
+  }
+
+  onSortedColumn(e: wjcGrid.CellRangeEventArgs) {
+    e.cancel = true;
+
+    if (this.filterColumns[e.col] !== this.sortColumn) {
+      this.sortColumn = this.filterColumns[e.col];
+      this.sortDirection = "ASC";
+    } else {
+      this.sortDirection = "DESC";
+    }
+
+    this.issueSearchRequest();
+  }
+
+  onFilterChanged(e: wjcGrid.CellRangeEventArgs) {
+    // any time a filter request is made, set the current page back to 1
+    this.currentPage = 1;
+
+    // cache the current filter as a typed JSON object
+    this.filterDefinitionCache = JSON.parse(this.filter.filterDefinition);
+
+    // cancel the event as we are going to issue a new search request
+    if (e.cancel !== true) {
+      e.cancel = true;
+
+      this.issueSearchRequest();
+    }
+  }
+
+  editRow(record: any): void {
+    // emit single record edit event
+    console.log('PagedSearchGrid - editRow', record);
+    this.editRecord.emit(record);
+  }
+
+  editRows() {
+    const records = this.flexGrid.selectedItems;
+    // emit multi record edit event
+  }
+
+  isMultipleRecordsSelected(): boolean {
+    return this.flexGrid.selectedItems.length > 1;
+  }
+
+  onPageRequest(pageRequested: number): void {
+    this.currentPage = pageRequested;
+
+    this.issueSearchRequest();
+  }
+
+  private setupAndConfigurGrid(): void {
+    if (this.showGrid) {
+      setTimeout(() => {
+        // get the list of column definitions
+        this.filterColumns = _.map(this.pagedSearchResult.columns, "columnId");         
+      }, 0);
 
       this.gridData = this.pagedSearchResult.searchResults;
+      
+      // set up the Value Filters for each column
+      setTimeout(() => {
+        this.setUpValueFilters();
+      }, 100);
+
       this.totalResults = this.pagedSearchResult.totalResults;
       this.currentPage = this.pagedSearchResult.page;
       this.totalPages = this.pagedSearchResult.totalPages;
       
-      this.showGrid = this.pagedSearchResult.searchResults.length > 0;
-    } 
+      this.gridSectionHeight = "500px";
+    } else {
+      // change the #flexGridSection section of the page to 0 height
+      this.gridSectionHeight = "0";
+    }
   }
 
-  private generateGridColumnCollection(columns: Column[]): ColumnDefinition[] {
-    let columnDefinitions: ColumnDefinition[] = [];
+  private issueSearchRequest(): void {
+    // set up search request, emit search request event
+    this.gridSearchRequest.emit(this.createSearchRequest());
+  }
 
-    _.forEach(columns, (column) => {
-      columnDefinitions.push({ header: column.columnHeader, binding: column.columnId, width: '*', format: column.format });
+  private resetGridSettingsToDefaults(): void {
+    this.sortColumn = PagedSearchDefaults.sortColumn;
+    this.sortDirection = PagedSearchDefaults.sortDirection;
+
+    this.filterDefinitionCache = null;
+  }
+
+  private createSearchRequest(): PagedSearchRequest {
+    return {
+      filters: this.getBindingFilters(),
+      sortBy: this.sortColumn,
+      sortDirection: this.sortDirection,
+      page: this.currentPage,
+      pageSize: PagedSearchDefaults.pageSize
+    }
+  }
+
+  private getBindingFilters(): ColumnSearchFilter[] {
+    const filters: ColumnSearchFilter[] = new Array<ColumnSearchFilter>();    
+
+    if (this.filterDefinitionCache && this.filterDefinitionCache.filters) {
+      _.forEach(this.filterDefinitionCache.filters, filter => {
+        if (filter.showValues) {
+          const values = ';' + Object.keys(filter.showValues).join(';') + ';';
+          filters.push({ binding: filter.binding, filter: values });
+        }
+      });
+    }
+
+    return filters;
+  }
+
+  private setUpValueFilters(): void {
+    _.forEach(this.filterColumns, column => {
+      const foundFilterList = _.find(this.pagedSearchResult.columns, (col) => { return col.columnId === column; });
+      const filterValues = foundFilterList.filterValues;
+      this.filter.getColumnFilter(column).valueFilter.uniqueValues = filterValues;
     });
-    
-    return columnDefinitions;
   }
 }
